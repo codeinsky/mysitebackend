c08b3d43a36b9190a9fcb8320be3df16ae23628c
Alex Todorov <alextodorov@alexs-mbp.mynet>
2020-03-24 00:38:41 +0200

    backend strted

---
+++ src/main/java/com/myweb/site/MyWebSiteApplication.java
@@ -0,0 +1,13 @@
+package com.myweb.site;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyWebSiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyWebSiteApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/myweb/site/beans/vote.java
@@ -0,0 +1,5 @@
+package com.myweb.site.beans;
+
+public class vote {
+
+}
